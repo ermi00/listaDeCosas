da4696cf0a7a2a903afb7ef9476a41dec290dbab
ermi00 <miguelgonzalez.280803@gmail.com>
2025-08-06 20:41:14 -0600

    diseño responsive, logica mejorada

--- index.js
+++ index.js
@@ -1,33 +1,54 @@
 let input = document.getElementById("input");
-let ul = document.getElementById("ul");
+let divContenido = document.getElementById("divContenido");
+let span = document.getElementById("span");
+let sonidoEliminar = new Audio("./assets/eliminar.wav");
+let agregarSonido = new Audio("./assets/agregar.wav");
+let visitas = localStorage.getItem("visitasPagina");
 
 function agregarElemento() {
-  let li = document.createElement("li");
+  let div = document.createElement("div");
   let nombreTarea = input.value;
+  let sonido = agregarSonido.cloneNode();
+  event.preventDefault();
 
   if (nombreTarea == "") {
-    alert("Ingresa una tarea antes");
+    span.innerHTML = "Intenta escribir algo primero";
     return;
   } else {
-    li.id = Math.random();
-    li.innerHTML = `${nombreTarea} <button onclick="eliminarElemento(${li.id})">quitar</button>`;
-    ul.appendChild(li);
-    localStorage.setItem(`${li.id}`, `${nombreTarea}`);
+    span.innerHTML = "...";
+    div.id = Math.random();
+    div.className = "divGenerico";
+    div.innerHTML = `<p>${nombreTarea}</p> <button onclick="eliminarElemento(${div.id})">ELIMINAR</button>`;
+    divContenido.appendChild(div);
+    localStorage.setItem(`${div.id}`, `${nombreTarea}`);
+    span.innerHTML = "Tarea agregada";
   }
+  sonido.play();
+  input.value = "";
 }
 
 function eliminarElemento(id) {
-  let li = document.getElementById(`${id}`);
-  li.remove();
+  let sonido = sonidoEliminar.cloneNode();
+  let div = document.getElementById(`${id}`);
+  div.remove();
   localStorage.removeItem(id);
+  sonido.play();
 }
 
-for (let i = 0; i < localStorage.length; i++) {
+for (
+  let i = 0;
+  i < localStorage.length && i != localStorage.key("visitasPagina");
+  i++
+) {
   let id = localStorage.key(i);
   let nombre = localStorage.getItem(id);
-
-  let li = document.createElement("li");
-  li.id = id;
-  li.innerHTML = `${nombre} <button onclick="eliminarElemento(${li.id})">quitar</button>`;
-  ul.appendChild(li);
+  span.innerHTML = `Parece que has estado aqui ${visitas} veces`;
+  let div = document.createElement("div");
+  div.id = id;
+  div.className = "divGenerico";
+  div.innerHTML = `<p>${nombre}</p><button onclick="eliminarElemento(${div.id})">ELIMINAR</button>`;
+  divContenido.appendChild(div);
 }
+
+visitas++;
+localStorage.setItem("visitasPagina", visitas);
